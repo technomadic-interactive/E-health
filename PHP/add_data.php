0192b40519a01e310b82789aaf2cbc48cb4a1416
Rogelio Ventura <rogeliovr94@gmail.com>
2017-06-06 17:38:57 -0500

    Prueba de variables y db en caso de emergencia

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -40,7 +40,7 @@
 
       if ($latitud){
           $peticion="INSERT INTO incidentes VALUES ('" .$IMEI."', NULL, $latitud, $longitud, '" .$fix."')"; 
-          include "/getAddressSms";
+          include "getAddressSms.php";
       }
 
     $link->query($peticion);
